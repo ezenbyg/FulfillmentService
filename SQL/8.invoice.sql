0f965af36e9ea3eb26ecb4e8fa3a38e9ea142296
BAEKJungHo <designjava@naver.com>
2019-05-07 16:57:51 +0900

    InvoiceModel

--- SQL/8.invoice.sql
+++ SQL/8.invoice.sql
@@ -1,7 +1,8 @@
 CREATE TABLE invoice (
   	vId int(5) unsigned NOT NULL AUTO_INCREMENT, 
 	vName varchar(10) NOT NULL DEFAULT "",
-	vTel varchar(15) NOT NULL DEFAULT "", 
+	vTel varchar(15) NOT NULL DEFAULT "",
+	vAddress varchar(3), 
 	vProductId int(5) unsigned NOT NULL,
 	vProductName varchar(100) NOT NULL DEFAULT "", 
 	vQuantity int(255) NOT NULL,
