b8c34ac3051978cae0b9fbf95e5ee4af5363a72f
BAEKJungHo <designjava@naver.com>
2019-05-08 11:16:57 +0900

    Pay

--- SQL/10.pay.sql
+++ SQL/10.pay.sql
@@ -3,6 +3,7 @@ CREATE TABLE pay (
 	yAdminId int(5) unsigned NOT NULL,
   	yPrice int(10) unsigned NOT NULL,
 	yDate datetime NOT NULL DEFAULT current_timestamp,
+	yState varchar(10) DEFAULT "ЙЬСіБо",
   	PRIMARY KEY (yId),
 	FOREIGN KEY (yAdminId) REFERENCES admins(aId)
 ) AUTO_INCREMENT=20000001 DEFAULT CHARSET=utf8;
